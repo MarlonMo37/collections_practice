eb5f9db988f96b91d3ddcc2611c4215636075404
MarlonMo37 <marlonmoreno894@gmail.com>
2020-10-29 15:48:34 -0500

    commit tree e82fe1e563fdede4a52d45b3a49275c321a6b8b9

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(numbers)
+    numbers.sort
+end
+
+def sort_array_desc(numbers)
+    numbers.sort do | left, right|
+        right <=> left
+    end
+end
+
+def sort_array_char_count(characters)
+    characters.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(element)
+    element[1], element[2] = element[2], element[1]
+    element
+end 
+
+def reverse_array(digits)
+    digits.reverse
+end
+
+def kesha_maker(money)
+    money.each {|mula| mula[2] = "$"}
+end
+
+def find_a(aaaa)
+    aaaa.select do |letter|
+        letter[0] == "a"
+    end  
+end
+
+def sum_array(num)
+    num.inject do |sum, n|
+        sum + n
+    end 
+end
+
+def add_s(words)
+    words.collect do |word|
+        if words[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
+
 
